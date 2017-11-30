690352ee15d200e8cb24c9a603cccd0b6eca5e79
Tyler <tdoermann@gmail.com>
2017-11-30 01:30:40 -0700

    added filter and sort to get most positive reviews

--- main.rb
+++ main.rb
@@ -1,7 +1,16 @@
 require './scraper'
 
+# Constants of 'overly positive'
+WOULD_RECOMMEND = "Yes"
+REVIEW_EMOTION = :positive
+SUM_IND_SCORE = 250
+
 scraper = Scraper.new("McKaig-Chevrolet-Buick-A-Dealer-For-The-People-dealer-reviews-23685")
 
-reviews = scraper.parse(1, 2)
+reviews = scraper.parse(1, 5)
+
+reviews = reviews.select { |review| review.wouldRecommend == WOULD_RECOMMEND and review.emotion == REVIEW_EMOTION and review.sumIndScore == SUM_IND_SCORE }
+
+reviews = reviews.sort{ |a, b|  a.polarity <=> b.polarity }
 
 puts reviews
