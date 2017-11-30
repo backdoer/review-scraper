ac13eb20db643995b496487eae3d124f646bd8ee
Tyler <tdoermann@gmail.com>
2017-11-30 00:05:29 -0700

    Created review class

---
+++ Review.rb
@@ -0,0 +1,51 @@
+require 'sentimental'
+
+class Review
+
+	@@analyzer = Sentimental.new
+	@@analyzer.load_defaults
+	@@analyzer.threshold = 2
+
+	def initialize(reviewContent, overallRating, individualRatings, wouldRecommend)
+		@reviewContent = reviewContent
+		@overallRating = overallRating
+		@individualRatings = individualRatings
+		@wouldRecommend = wouldRecommend
+	end
+
+	# To String
+	def to_s
+		
+	end
+
+	# Getters
+	def reviewContent
+		@reviewContent
+	end
+
+	def overallRating
+		@overallRating
+	end
+
+	def individualRatings
+		@individualRatings
+	end
+
+	def wouldRecommend
+		@wouldRecommend
+	end
+
+	# Calculated Fields
+	def polarity
+		return @@analyzer.score(@reviewContent)
+	end
+
+	def emotion
+		return @@analyzer.sentiment(@reviewContent)
+	end
+
+	def sumIndScore
+		return @individualRatings.values.inject(:+)
+	end
+
+end
